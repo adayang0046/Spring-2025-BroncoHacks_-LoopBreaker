1d0ec4c1d122e926568e9694eb678cd1ab5914c9
[Ruiyang Wang] <[wangruiyang1210@gmail.com]>
2025-04-19 09:59:59 -0700

    Leverage Gemini

--- project/chatbot/views.py
+++ project/chatbot/views.py
@@ -2,9 +2,33 @@ import copy
 import os 
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json 
+import google.generativeai as genai 
+from dotenv import load_dotenv
+
+# Load API key from.env
+load_dotenv()
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel("models/gemini-1.5-pro-latest")
+
+def chat_bot(request):
+    return render(request, 'chatbot.html')
 
 # Render Landing Webpage
-def chat_bot(request):
+@csrf_exempt
+def ask_williams(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        user_question = data.get("question", "")
 
+        try:
+            response = model.generate_content(user_question)
+            return JsonResponse({"response": response.text})
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+    else:
+        return JsonResponse({"error": "Only POST allowed."}, status=405)
 
-    return render(request, 'chatbot.html')
